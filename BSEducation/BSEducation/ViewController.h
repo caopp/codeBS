20527ac8bc744e3034e4748f5d587e3ddf46819c
caopengpeng <642412120@qq.com>
2016-12-05 15:37:35 +0800

    创建
    创建工程

---
+++ BSEducation/BSEducation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSEducation
+//
+//  Created by caopenpen on 16/12/5.
+//  Copyright © 2016年 bs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BSEducation/BSEducation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BSEducation
+//
+//  Created by caopenpen on 16/12/5.
+//  Copyright © 2016年 bs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
